524b3adfa05b1c9d30c5c2c86a1a66b1c36c741f
phuongha2012 <hami201289@him-mail.ch>
2019-08-15 20:32:41 +1200

    configure authentication JwtBearerDefault

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -41,7 +43,7 @@ namespace DatingApp.API.Controllers
         {
         }
 
-        // PUT api/values/5
+        [AllowAnonymous]
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
